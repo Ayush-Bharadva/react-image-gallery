c1bf40aa0c482f641ce610911440c31ffd0d4720
Ayush-Bharadva <ayushkumar.bharadva@zuru.com>
2024-02-05 10:46:09 +0530

    recovered constants

---
+++ src/constants/constants.js
@@ -0,0 +1,1 @@
+export const relatedCategories = ["people", "nature", "funny", "sports", "dice", "abstract", "background", "photo", "design", "dark", "business", "technology", "coding", "programming", "art"];
